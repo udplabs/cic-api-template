18b193271170d008a83d73c869a8962bd5417788
Danny Fuhriman <eatplaysleep@gmail.com>
2023-05-24 09:32:41 -0600

    abstract function

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
-import { } from '@okta/jwt-verifier';
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 import { existsSync } from 'fs';
-import JwtVerifier from './utils/jwtVerifier.js'
+
+import { verifyJwt } from './utils/verifyJwt.js';
 import config from './config.js';
 
 export const loadEnv = (options) => {
@@ -20,103 +18,20 @@ export const loadEnv = (options) => {
 
 loadEnv();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const { auth, server } = config || {};
+const { server } = config || {};
 
 const {
-	AUTH_CLIENT_ID: clientId = auth?.clientId,
-	AUTH_DOMAIN: domain = auth?.domain,
-	AUTH_ISSUER: ISSUER = auth?.issuer,
-	SERVER_AUDIENCE: AUDIENCE = server?.audience || auth?.audience,
 	SERVER_AUTH_PERMISSIONS: AUTH_PERMISSIONS = server?.permissions || [],
 	PORT: port,
 } = process.env;
 
 const permissions = Array.isArray(AUTH_PERMISSIONS) ? AUTH_PERMISSIONS : AUTH_PERMISSIONS.split(' ');
 
-const audience = Array.isArray(AUDIENCE)
-	? AUDIENCE
-	: AUDIENCE?.includes(', ')
-		? AUDIENCE?.split(', ')
-		: AUDIENCE?.split(',');
-
-const issuer =
-	ISSUER || domain.lastIndexOf('/') === domain.length - 1 ? 'https://' + domain : 'https://' + domain + '/';
-
 const app = express();
 
 app.use(cors({ origin: '*' }));
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(express.static(join(__dirname, 'public')));
-
-const verifyJwt = (options) => {
-	if (audience && options?.audience?.includes('not_configured')) {
-		delete options.audience;
-	}
-
-	options = {
-		issuer,
-		clientId,
-		audience,
-		...options,
-	};
-
-	const verifier = new JwtVerifier(options);
-
-	const verifyToken = async (req, res, next) => {
-		try {
-			if (req?.method === 'OPTIONS' && req.get('access-control-request-headers')) {
-				console.log('doing OPTIONS');
-				const hasAuthInAccessControl = req
-					.get('access-control-request-headers')
-					?.split(',')
-					?.map((header) => header?.trim()?.toLowerCase())
-					?.includes('authorization');
-
-				if (hasAuthInAccessControl) {
-					return next();
-				}
-			}
-			const authHeader = req.get('authorization');
-			const match = authHeader?.match(/Bearer (.+)/) || [];
-
-			if (match.length < 1) {
-				return res.status(401).send('Unable to parse `Authorization` header');
-			}
-
-			const accessToken = match[1];
-
-			req.jwt = await verifier.verifyToken(accessToken, {
-				audience: options?.audience,
-			});
-
-			next();
-		} catch (error) {
-			const response = {
-				success: false,
-				message: error?.message,
-			};
-
-			if (error?.message.includes('|')) {
-				response.status = error.message.split('|')[0];
-				response['errorDetails'] = JSON.parse(error.message.split('|')[1]);
-				delete response.message;
-			}
-
-			if (error?.details?.includes('|')) {
-				response.status = error.details.split('|')[0];
-				response['errorDetails'] = error.details.split('|')[1];
-			}
-
-			res.status(401).json(response);
-		}
-	};
-
-	return verifyToken;
-};
 
 app.get('/api/public', (req, res) => {
 	res.json({
@@ -126,7 +41,7 @@ app.get('/api/public', (req, res) => {
 }
 );
 
-app.get('/api/private', verifyJwt({ audience: [audience] }), (req, res) =>
+app.get('/api/private', verifyJwt({ audience: ['not_configured'] }), (req, res) =>
 	res.json({
 		success: true,
 		message:
